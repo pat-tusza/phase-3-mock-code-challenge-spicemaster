217ea56c577c8cac7101f5e8c710aac1a24ec938
Patrick Tuszakowski <mrtall500@gmail.com>
2021-03-03 10:08:37 -0500

    first Deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 // write your code here
+
+
+const div = document.querySelector('div#spice-blend-detail')
+const spiceUrl = 'http://localhost:3000/spiceblends'
+const ingredUrl = 'http://localhost:3000/ingredients'
+
+function spiceHtml(spice){
+    const img = div.querySelector('img')
+    const h2 = div.querySelector('h2')
+    img.src = spice[0].image
+    img.alt = spice[0].title
+    h2.innerText = `${spice[0].title}`
+}
+
+function ingredHtml(ingreds){
+    const ul = div.querySelector('ul')
+    ingreds.forEach(ingred => {
+        if (ingred.spiceblendId == 1) {
+            const li = document.createElement('li')
+            li.innerText = `${ingred.name}`
+            ul.append(li)
+        }
+    })
+}
+
+function loadSpice(){
+    fetch(spiceUrl)
+    .then(res => res.json())
+    .then(spice => spiceHtml(spice))
+}
+
+function loadIngred(){
+    fetch(ingredUrl)
+    .then(res => res.json())
+    .then(ingred => ingredHtml(ingred))
+}
+function loadDb(){
+    loadSpice()
+    loadIngred()
+}
+
+
+
+
+
+
+
+
+
+loadDb()
